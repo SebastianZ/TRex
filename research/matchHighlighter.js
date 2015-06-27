637e4dfad0ef1f17a5b66d37ad251d4b6f9ce614
Farshid Beheshti <farshid.beheshti@gmail.com>
2015-06-27 12:58:51 +0430

    Integrate the regex and matches highlighters into the simple UI.

--- research/matchHighlighter.js
+++ research/matchHighlighter.js
@@ -29,26 +29,61 @@
     return highlightedSearchText;
   };
 
-  prototype.highlight = function() {
-    var matches = [
-      {
-        start: 3,
-        end: 8
-      },
-      {
-        start: 14,
-        end: 15
+  prototype.clearHighlight = function () {
+      var childNodes = this.field.childNodes;
+      for (var i = 0, len = childNodes.length; i < len; i++) {
+          if (childNodes[i].nodeType == Node.ELEMENT_NODE) {
+              childNodes[i].classList.remove("match");
+          }
       }
-    ];
+  };
 
-    var selectionOffset = this.getSelectionOffset(this.field);
-    var searchText = this.field.textContent;
-    this.field.textContent = "";
-    this.field.appendChild(this.highlightMatches(searchText, matches));
-    var selection = window.getSelection();
-    selection.removeAllRanges();
-    var range = this.getRangeByOffset(this.field, selectionOffset);
-    selection.addRange(range);
+  prototype.matches = [];
+
+  prototype.highlight = function () {
+      var searchText = this.field.textContent;
+      var regexPattern = window.regExpField.textContent;
+      var regex = null;
+      this.matches = [];
+      // xxxFarshid: Would there be a better way instead of wrapping
+      // into the try...catch block?
+      try {
+          regex = new RegExp(regexPattern, "g");
+      }
+      catch (e) { }
+
+      var match = null;
+      var lastIndex = -1;
+      if (regex && regexPattern) {
+          while ((match = regex.exec(searchText)) != null) {
+
+              // NOTE: It's weird that the next match with exec() func on a RegExp
+              // with some patterns like "a|" never finish (goes into an infinite loop).
+              if (lastIndex == regex.lastIndex) {
+                  this.matches = [];
+                  break;
+              }
+              lastIndex = regex.lastIndex;
+
+              this.matches.push({
+                  start: match.index,
+                  end: match.index + match[0].length
+              });
+          }
+      }
+
+      if (!this.matches.length) {
+          this.clearHighlight();
+          return;
+      }
+
+      var selectionOffset = this.getSelectionOffset(this.field);
+      this.field.textContent = "";
+      this.field.appendChild(this.highlightMatches(searchText, this.matches));
+      var selection = window.getSelection();
+      selection.removeAllRanges();
+      var range = this.getRangeByOffset(this.field, selectionOffset);
+      selection.addRange(range);
   }
 
   window.MatchHighlighter = MatchHighlighter;
